248f5989f1ee96be5d299c655b323f53b14b3b40
Mingjia Wang <mingjiawangdev@gmail.com>
2023-05-14 09:33:34 -0700

    Reset pages to have 1 meme each

--- playground/src/memes/memes.js
+++ playground/src/memes/memes.js
@@ -1,51 +1,27 @@
 // import norcal memes here
-import pic1 from './meme-images/norcal.png';
-import pic2 from './meme-images/socal.png';
-import pic3 from './meme-images/international.png';
 import thisIsFine from './meme-images/this-is-fine.jpeg';
 
 // import socal memes here
-import pic4 from './meme-images/norcal.png';
-import pic5 from './meme-images/socal.png';
-import pic6 from './meme-images/international.png';
-import cuteKid from './meme-images/cute_kid.webp';
 import lateToWork from './meme-images/late_to_work.webp';
-import tired from './meme-images/tired.png';
 
 // import out of state memes here
-import pic7 from './meme-images/norcal.png';
-import pic8 from './meme-images/socal.png';
-import pic9 from './meme-images/international.png';
+import tired from './meme-images/tired.png';
 
 // import international memes here
-import pic10 from './meme-images/norcal.png';
-import pic11 from './meme-images/socal.png';
-import pic12 from './meme-images/international.png';
+import butterfly from './meme-images/butterfly.jpeg';
 
 const memeData = {
     norcalMemes: [
-        pic1,
-        pic2,
-        pic3,
         thisIsFine
     ],
     socalMemes: [
-        pic4,
-        pic5,
-        pic6,
-        cuteKid,
-        lateToWork,
-        tired
+        lateToWork
     ],
     outOfStateMemes: [
-        pic7,
-        pic8,
-        pic9
+        tired,
     ],
     internationalMemes: [
-        pic10,
-        pic11,
-        pic12
+        butterfly
     ],
 };
 
